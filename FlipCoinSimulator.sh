09a2fed91a3fbfc91c5daa5e04173dc8120c3572
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-28 11:32:51 +0530

    ADDED : Display doublet percentage

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -3,6 +3,7 @@
 declare -A singlet
 declare -A doublet
 singlet=( ["H"]=0 ["T"]=0 )
+doublet=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0 )
 function check()
 {
 	toss=$((RANDOM%2))
@@ -31,11 +32,50 @@ function flipCoin()
 			then
 				singlet[T]=$(( ${singlet[T]}+1 ))
 			fi
+		elif [[ $numberOfCoin -eq 2 ]]
+		then
+			check
+	      temp1=$result
+   	   check
+      	temp2=$result
+        	if [[ $temp1 -eq 1 && $temp2 -eq 1 ]]
+         then
+            doublet[HH]=$(( ${doublet[HH]}+1 ))
+         elif [[ $temp1 -eq 0 && $temp2 -eq 0 ]]
+         then
+            doublet[TT]=$(( ${doublet[TT]}+1 ))
+         elif [[ $temp1 -eq 1 && $temp2 -eq 0 ]]
+         then
+            doublet[HT]=$(( ${doublet[HT]}+1 ))
+         elif [[ $temp1 -eq 0 && $temp2 -eq 1 ]]
+         then
+            doublet[TH]=$(( ${doublet[TH]}+1 ))
+         fi
 		fi
 	done
-	echo ${singlet[@]}
-	echo ${!singlet[@]}
-	headPercent=`expr "scale=2; ( ${singlet[H]} / $number *100 ) " | bc -l`
-	echo "percentage=$headPercent%"
+	if [[ $numberOfCoin -eq 1 ]]
+   then
+		echo ${singlet[@]}
+   	echo ${!singlet[@]}
+   	headPercent=`expr "scale=2; ( ${singlet[H]} / $number *100 ) " | bc -l`
+   	echo "percentage=$headPercent%"
+		tailPercent=`expr "scale=2; ( ${singlet[T]} / $number *100 ) " | bc -l`
+      echo "Percentage=$tailPercent%"
+
+	elif [[ $numberOfCoin -eq 2 ]]
+	then
+		echo ${doublet[@]}
+      echo ${!doublet[@]}
+      headHead=`expr "scale=2; ( ${doublet[HH]} / $number *100 ) " | bc -l`
+      echo "Percentage of HH =$headHead%"
+		tailTail=`expr "scale=2; ( ${doublet[TT]} / $number *100 ) " | bc -l`
+      echo "Percentage of TT=$tailTail%"
+		headTail=`expr "scale=2; ( ${doublet[HT]} / $number *100 ) " | bc -l`
+      echo "Percentage of HT =$headTail%"
+      tailHead=`expr "scale=2; ( ${doublet[TH]} / $number *100 ) " | bc -l`
+      echo "Percentage of TT=$tailHead%"
+
+
+	fi
 }
 flipCoin
